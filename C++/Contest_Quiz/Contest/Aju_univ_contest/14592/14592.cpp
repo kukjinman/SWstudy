3ff27d31f06379bcca53f098d07b36374409ee70
kukjin.chung <thater114@gmail.com>
2021-11-08 14:30:45 +0900

    [SWstudy][C++][Contest_Quiz][Contest] 14592 Update
    [DESC][0] Aju_univ_contest 14592 solved

---
+++ C++/Contest_Quiz/Contest/Aju_univ_contest/14592/14592.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+void print_queue(T q)
+{ // NB: pass by value so the print uses a copy
+
+    cout << "----------------------------" << endl;
+
+    while (!q.empty()) {
+        std::cout << "[p] " << q.top() << ' ';
+        q.pop();
+    }
+    std::cout << '\n';
+
+    cout << "----------------------------" << endl;
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N = 0;
+    cin >> N;
+
+    int S[3] = {
+        -1,
+    };
+
+    int C[3] = {
+        -1,
+    };
+
+    int L[3] = {
+        -1,
+
+    };
+
+    for (int i = 0; i < N; i++) {
+        cin >> S[i] >> C[i] >> L[i];
+    }
+
+    int best = 0;
+
+    int S_best = 0;
+    int C_best = 0;
+    int L_best = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] > S_best) {
+            S_best = S[i];
+            C_best = C[i];
+            L_best = L[i];
+            best = i;
+        } else if (S[i] == S_best) {
+
+            if (C[i] < C_best) {
+                S_best = S[i];
+                C_best = C[i];
+                L_best = L[i];
+                best = i;
+            } else if (C[i] == C_best) {
+
+                if (L[i] < L_best) {
+                    S_best = S[i];
+                    C_best = C[i];
+                    L_best = L[i];
+                    best = i;
+                }
+            }
+        }
+    }
+
+    cout << best + 1 << endl;
+
+    return 0;
+}
