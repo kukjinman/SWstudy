ba5b324e851d3c045d8b965c30616202ebcde076
kukjin.chung <thater114@gmail.com>
2021-12-07 20:13:52 +0900

    [SWstudy][Curl] curl url setting and cookiefile set
    [DESC][0] google.com is working
    [DESC][1] CURLOPT_COOKIEFILE does not store any file.

--- Curl_Study/CURLOPT_SHARE.cpp
+++ Curl_Study/CURLOPT_SHARE.cpp
@@ -13,15 +13,15 @@ int main(int argc, char** argv)
         CURLSH* shobject = curl_share_init();
         curl_share_setopt(shobject, CURLSHOPT_SHARE, CURL_LOCK_DATA_COOKIE);
 
-        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
-        curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://google.com/");
+        curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "/home/kukjin.chung/SWstudy/github/SWstudy/Curl_Study/cookie");
         curl_easy_setopt(curl, CURLOPT_SHARE, shobject);
         int ret = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
 
         /* the second handle shares cookies from the first */
         curl_easy_setopt(curl2, CURLOPT_URL, "https://example.com/second");
-        curl_easy_setopt(curl2, CURLOPT_COOKIEFILE, "");
+        curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "/home/kukjin.chung/SWstudy/github/SWstudy/Curl_Study/cookie");
         curl_easy_setopt(curl2, CURLOPT_SHARE, shobject);
         ret = curl_easy_perform(curl2);
         curl_easy_cleanup(curl2);
