26541593e964d25d66f60b42cd2e070f44b52b26
kukjin.chung <thater114@gmail.com>
2021-12-16 16:14:08 +0900

    [SWstudy][Cmake] Cmake code
    [DESC][0] Cmake test code applied

---
+++ Cmake_Study/3_Cmake_basic_example/build/CMakeFiles/CMake_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/bar.c" "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/build/CMakeFiles/CMake_test.dir/bar.c.o"
+  "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/foo.c" "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/build/CMakeFiles/CMake_test.dir/foo.c.o"
+  "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/main.c" "/home/kukjin.chung/SWstudy/github/SWstudy/Cmake_Study/3_Cmake_basic_example/build/CMakeFiles/CMake_test.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cmake_Study/3_Cmake_basic_example/build/CMakeFiles/CMake_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMake_test.dir/main.c.o"
+  "CMakeFiles/CMake_test.dir/foo.c.o"
+  "CMakeFiles/CMake_test.dir/bar.c.o"
+  "CMake_test.pdb"
+  "CMake_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMake_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
