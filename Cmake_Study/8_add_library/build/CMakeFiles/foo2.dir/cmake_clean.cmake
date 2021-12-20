3319effb2e40509b2257bee0a50cb203a8253968
kukjin.chung <thater114@gmail.com>
2021-12-20 16:49:18 +0900

    [SWstudy][Cmake] CMake add_executable tested
    [DESC][0] add_executable and add_library tested
    [DESC][1] Makefile is added

---
+++ Cmake_Study/8_add_library/build/CMakeFiles/foo2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foo2.dir/foo.c.o"
+  "libfoo2.pdb"
+  "libfoo2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/foo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
