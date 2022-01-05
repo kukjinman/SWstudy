8cfba3497b296072c0c32d2f68c93fa8f08fe47c
kukjin.chung <thater114@gmail.com>
2022-01-05 16:53:56 +0900

    [SWstudy][C++][SegmentTree] 10868 commit
    [DESC][0] this is not solved. sol code is updated first.

---
+++ Algorithm/Baekjun/C++/C++Algorithm/SegmentTree/10868.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+int n = 0, m = 0;
+#define max 100001
+
+int num[max];
+
+void input()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    input();
+
+    return 0;
+}
