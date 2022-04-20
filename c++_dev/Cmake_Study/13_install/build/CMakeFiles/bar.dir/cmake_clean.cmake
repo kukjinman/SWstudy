ff5c3ab6c55a70a9c439bfe1b8bb75a9c0ebec95
kukjin.chung <thater114@gmail.com>
2022-04-20 17:05:52 +0900

    [SWstudy][c++_dev][cmake] 13 install 2nd commit
    [DESC][0] purpose of install is not figured out.

---
+++ c++_dev/Cmake_Study/13_install/build/CMakeFiles/bar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bar.dir/bar/bar.c.o"
+  "libbar.pdb"
+  "libbar.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
