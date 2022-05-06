60cca64d23e2c47bb33834a3e9043f0291e11de3
kukjin.chung <thater114@gmail.com>
2022-05-06 18:39:05 +0900

    [SWstudy][Algorithm] k진수에서소수개수구하기 commit
    [DESC][0] c++ solution added

---
+++ Algorithm/Programmers/k진수에서소수개수구하기/sol_failed.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool PrimeChecker(string s_)
+{
+    //소수 체크 코드는 외울 것
+    int ret = false;
+
+    if (s_.empty() || s_ == "1")
+        return false;
+
+    long long num = stol(s_);
+
+    for (long long i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    ret = true;
+    return ret;
+}
+
+int solution(int n, int k)
+{
+    int answer = 0;
+    int cur = n;
+    string s;
+
+    if (k == 0) {
+        return answer;
+    }
+
+    while (cur > 0) {
+        // int tmp = cur % k;
+        // cout << "tmp :" << tmp << endl;
+
+        s += to_string(cur % k);
+        // cout << " cur : " << cur << endl;
+        cur = cur / k;
+        // cout << " cur : " << cur << endl;
+    }
+
+    reverse(s.begin(), s.end());
+
+    string buf = "";
+
+    // for (int i = 0; i < s.size(); i++) {
+
+    //     if (s[i] == '0') {
+    //         if (!buf.empty()) {
+
+    //             if (PrimeChecker(buf)) {
+    //                 answer++;
+    //             }
+
+    //             buf = "";
+    //             continue;
+    //         }
+    //     }
+    //     buf += s[i];
+    // }
+
+    for (char c : s) {
+
+        if (c == '0') {
+
+            if (!buf.empty() && PrimeChecker(buf)) {
+                answer++;
+            }
+
+            buf = "";
+            continue;
+        }
+        buf += c;
+    }
+
+    if (!buf.empty() && PrimeChecker(buf)) {
+        answer++;
+    }
+
+    return answer;
+}
