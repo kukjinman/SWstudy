75339229ca1f814678d60b3870d8c0abbcfda85b
kukjin.chung <thater114@gmail.com>
2022-05-26 15:37:32 +0900

    [SWstudy][Thread] Thread Pool test main updated
    [DESC][0] test main is updated

--- c++_dev/Thread_Study/ThreadPool/UnitTest/test_main.cpp
+++ c++_dev/Thread_Study/ThreadPool/UnitTest/test_main.cpp
@@ -1,14 +1,26 @@
 #include "../ThreadPool_class/ThreadPool.h"
 #include <iostream>
 #include <memory>
-// using namespace std;
+using namespace std;
 
 int main()
 {
-    std::cout << "Unit test main start" << std::endl;
+    cout << "Unit test main start" << endl;
     std::shared_ptr<ThreadPool::ThreadPool> m_ThreadPool;
     m_ThreadPool = std::make_shared<ThreadPool::ThreadPool>(10);
 
+    cout << "=======================" << endl;
+    cout << "  Input Test Number " << endl;
+    cout << "=======================" << endl;
+
+    cout << "Type q to exit" << endl;
+
+    cout << "1 : " << endl;
+    cout << "2 : " << endl;
+    cout << "3 : " << endl;
+    cout << "4 : " << endl;
+
     
+
     return 0;
 }
