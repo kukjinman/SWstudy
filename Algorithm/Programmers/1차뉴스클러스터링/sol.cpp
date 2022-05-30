e359cfc06f7a9f7263fb27b04a7519e61bfc1182
kukjin.chung <thater114@gmail.com>
2022-05-30 10:43:37 +0900

    [SWstudy][Algorithm] 1차뉴클러스터링 commit
    [DESC][0] sol code is updated

--- Algorithm/Programmers/1차뉴스클러스터링/sol.cpp
+++ Algorithm/Programmers/1차뉴스클러스터링/sol.cpp
@@ -31,5 +31,80 @@ int solution(string str1, string str2)
 
     // cout << " str2_low : " << str2_low << endl;
 
+    for (int i = 1; i < str1_low.size(); i++) {
+
+        // cout << "[str1] 1 : " << str1_low[i - 1] << " 2 : " << str1_low[i] << endl;
+
+        if (str1_low[i - 1] >= 'a' && str1_low[i - 1] <= 'z' && str1_low[i] >= 'a' && str1_low[i] <= 'z') {
+            string tmp = "";
+            tmp += str1_low[i - 1];
+            tmp += str1_low[i];
+            mp1[tmp]++;
+
+            // cout << "tmp1 : " << tmp << endl;
+
+        } else {
+        }
+    }
+
+    for (int i = 1; i < str2_low.size(); i++) {
+
+        // cout << " 1 : " << str2_low[i - 1] << " 2 : " << str2_low[i] << endl;
+
+        if (str2_low[i - 1] >= 'a' && str2_low[i - 1] <= 'z' && str2_low[i] >= 'a' && str2_low[i] <= 'z') {
+            string tmp = "";
+            tmp += str2_low[i - 1];
+            tmp += str2_low[i];
+            mp2[tmp]++;
+
+            // cout << "tmp2 : " << tmp << endl;
+
+        } else {
+        }
+    }
+
+    int intersec_cnt = 0;
+    int union_cnt = 0;
+
+    int mp1_sum = 0;
+    int mp2_sum = 0;
+
+    //교집합
+    for (auto it : mp1) {
+        cout << " first : " << it.first << " second : " << it.second << endl;
+
+        if (mp2[it.first] >= 1) {
+
+            if (mp1[it.first] > mp2[it.first])
+                intersec_cnt += mp2[it.first];
+            else {
+                intersec_cnt += mp1[it.first];
+            }
+        }
+
+        mp1_sum += it.second;
+    }
+
+    for (auto it : mp2) {
+        cout << " first : " << it.first << " second : " << it.second << endl;
+
+        mp2_sum += it.second;
+    }
+
+    cout << " mp1 : " << mp1.size() << " mp2 : " << mp2.size() << endl;
+    cout << " mp1_sum  : " << mp1_sum << " mp2_sum : " << mp2_sum << endl;
+
+    if (mp1.size() + mp2.size() == 0) {
+        if (str1_low == str2_low) {
+            answer = 65536;
+            return answer;
+        }
+    }
+
+    if ((intersec_cnt + mp1_sum + mp2_sum) != 0) {
+        double tmp = 0;
+        tmp = (double)intersec_cnt / (double(mp1_sum + mp2_sum - intersec_cnt));
+        answer = tmp * 65536;
+    }
     return answer;
 }
