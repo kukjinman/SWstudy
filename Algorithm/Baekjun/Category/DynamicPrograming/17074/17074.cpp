c07a6aca133a8a5849071bf7c4292633f195f56a
kukjin.chung <thater114@gmail.com>
2022-08-15 16:33:23 +0900

    [SWstudy][Algorithm][DP] 17074 commit [DESC][0] This is not solved

---
+++ Algorithm/Baekjun/Category/DynamicPrograming/17074/17074.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int n = 0;
+
+int arr[100001] = { 0, };
+
+int main(int argc, char* argv[])
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+
+
+
+    return 0;
+}
