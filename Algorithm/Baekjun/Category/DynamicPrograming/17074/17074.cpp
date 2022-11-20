41068e5080833045bf2ad85506b3b69bcf9474f7
kukjin.chung <thater114@gmail.com>
2022-11-20 17:30:13 +0900

    [SWstudy][Algorithm][DP] 17074 commit [DESC][0] This is solved

--- Algorithm/Baekjun/Category/DynamicPrograming/17074/17074.cpp
+++ Algorithm/Baekjun/Category/DynamicPrograming/17074/17074.cpp
@@ -1,21 +1,89 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int n = 0;
 
-int arr[100001] = { 0, };
+vector<long long> arr;
 
 int main(int argc, char* argv[])
 {
     cin >> n;
+    int cnt = 0;
+
+    long long prev = -1000000001;
+    int critical_index = 0;
+    int inverse_cnt = 0;
 
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+
+        long long cur = 0;
+        cin >> cur;
+        arr.push_back(cur);
+
+
+    }
+
+    for (int i = 0; i < n -1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            critical_index = i;
+            inverse_cnt++;
+ //           cout << " i : " << i << " inv : " << inverse_cnt << endl;
+        }
+     
     }
 
 
+    if (inverse_cnt == 0)
+    {
+        cout << n;
+    }
+    else if (inverse_cnt == 1)
+    {
+        if (critical_index == n - 2)
+        {
+            if (arr[critical_index - 1] <= arr[critical_index + 1])
+            {
+                cout << 2;
+            }
+            else {
+                cout << 1;
+            }
+        }
+        else if (critical_index == 0)
+        {
+            if (arr[critical_index] <= arr[critical_index + 2])
+            {
+                cout << 2;
+            }
+            else {
+                cout << 1;
+            }
+
+        }
+        else {
+            int cnt = 0;
+            if (arr[critical_index - 1] <= arr[critical_index + 1])
+            {
+                cnt++;
+            }
+
+            if (arr[critical_index] <= arr[critical_index + 2])
+            {
+                cnt++;
+            }
+            cout << cnt;
+        }
+
+
+    }
+    else {
+        cout << 0;
+    }
 
 
     return 0;
