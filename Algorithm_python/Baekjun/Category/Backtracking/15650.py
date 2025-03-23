583da27526fb9f297a0cec19dc7564b6eab23877
kukjinm <thater114@gmail.com>
2025-03-23 11:03:28 +0900

    [python 역량인증] 6603 commit

--- Algorithm_python/Baekjun/Category/Backtracking/15650.py
+++ Algorithm_python/Baekjun/Category/Backtracking/15650.py
@@ -3,11 +3,11 @@ n,m= map(int,input().split())
 
 arr = []
 
-def dfs(cnt):
+def dfs(depth):
     if len(arr) == m:
         print(' '.join(map(str,arr)))
         return
-    for i in range(cnt,n+1):
+    for i in range(depth,n+1):
         if i not in arr:
             arr.append(i)
             dfs(i+1)
--- Algorithm_python/Baekjun/Category/Backtracking/15662.py
+++ Algorithm_python/Baekjun/Category/Backtracking/15662.py
@@ -3,12 +3,12 @@ n,m= map(int,input().split())
 
 arr = []
 
-def dfs(cnt):
+def dfs(depth):
     if len(arr) == m:
         print(' '.join(map(str,arr)))
         return
 
-    for i in range(cnt,n+1):
+    for i in range(depth,n+1):
         arr.append(i)
         dfs(i)
         arr.pop()
