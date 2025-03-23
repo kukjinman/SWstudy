bd4981847d476470137c36279e64ab922fb0d00c
kukjinm <thater114@gmail.com>
2025-03-23 10:43:40 +0900

    [python 역량인증] 15650 15662 commit

---
+++ Algorithm_python/Baekjun/Category/Backtracking/15650.py
@@ -0,0 +1,16 @@
+
+n,m= map(int,input().split())
+
+arr = []
+
+def dfs(cnt):
+    if len(arr) == m:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(cnt,n+1):
+        if i not in arr:
+            arr.append(i)
+            dfs(i+1)
+            arr.pop()
+
+dfs(1)
---
+++ Algorithm_python/Baekjun/Category/Backtracking/15662.py
@@ -0,0 +1,17 @@
+
+n,m= map(int,input().split())
+
+arr = []
+
+def dfs(cnt):
+    if len(arr) == m:
+        print(' '.join(map(str,arr)))
+        return
+
+    for i in range(cnt,n+1):
+        arr.append(i)
+        dfs(i)
+        arr.pop()
+
+
+dfs(1)
